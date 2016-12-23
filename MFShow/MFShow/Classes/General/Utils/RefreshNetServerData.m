57fc933a38a27ca993dc88da10eaecd63e8fb6f3
Your Name <LittleDream92>
2016-12-23 17:45:50 +0800

    MFShow

---
+++ MFShow/MFShow/Classes/General/Utils/RefreshNetServerData.m
@@ -0,0 +1,13 @@
+//
+//  RefreshNetServerData.m
+//  MFShow
+//
+//  Created by Meng Fan on 16/12/23.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import "RefreshNetServerData.h"
+
+@implementation RefreshNetServerData
+
+@end
--- MFShow/MFShow/Classes/Section/Home/Controller/HomeViewController.m
+++ MFShow/MFShow/Classes/Section/Home/Controller/HomeViewController.m
@@ -10,7 +10,7 @@
 #import "NavigationTitleButton.h"
 #import "MainFeedViewController.h"
 
-@interface HomeViewController () <NavigationTitleButtonDelegate>
+@interface HomeViewController () <NavigationTitleButtonDelegate, UINavigationControllerDelegate>
 
 //导航栏自定义控件
 @property (weak, nonatomic) IBOutlet NavigationTitleButton *titleButton;
@@ -28,9 +28,25 @@
     [self setUpViews];
 }
 
+#pragma mark - systemViewMethods
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+        if (self.navigationController.navigationBar) {
+            self.navigationController.navigationBar.translucent = NO;
+        }
+        self.view.clipsToBounds = NO;
+    }
+
+}
+
 
 #pragma mark - setUpViews
 - (void)setUpNav {
+    self.navigationController.delegate = self;
+    
     //设置导航栏代理
     self.titleButton.delegate = self;
     //初始化选中下标
@@ -53,6 +69,49 @@
     NSLog(@"selectIndex: %ld", [selectedIndex integerValue]);
 }
 
+
+#pragma mark - UINavigationControllerDelegate
+-(void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    if (viewController ==  self) {
+        UIApplication* app = [UIApplication sharedApplication];
+        if (app.statusBarHidden) {
+            app.statusBarHidden = NO;
+        }
+        if (navigationController.navigationBar.hidden) {
+            navigationController.navigationBarHidden = NO;
+        }
+        if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+            [navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+            navigationController.navigationBar.translucent = NO;
+            navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+            navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
+//            [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+        }
+        else {
+//            [navigationController.navigationBar setBackgroundImage:stretchableImage(@"navigation_tab_2_p") forBarMetrics:UIBarMetricsDefault];
+        }
+    }
+    
+    else {
+        UIApplication* app = [UIApplication sharedApplication];
+        if (app.statusBarHidden) {
+            app.statusBarHidden = NO;
+        }
+        if (navigationController.navigationBar.hidden) {
+            navigationController.navigationBarHidden = NO;
+        }
+        //        if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+        //            [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+        //        }
+        //        [navigationController.navigationBar setBackgroundImage:stretchableImage(@"navigationbar_bg_white_6.png") forBarMetrics:UIBarMetricsDefault];
+        [navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+        navigationController.navigationBar.translucent = NO;
+        navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+        navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
+ //       [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+    }
+    
+}
 
 
 #pragma mark -
