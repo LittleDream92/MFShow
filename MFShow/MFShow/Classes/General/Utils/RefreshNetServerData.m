cf6233ea6304e7d4f3a6add003379028d4aa258f
Your Name <LittleDream92>
2016-12-26 10:16:10 +0800

    MFShow

--- MFShow/MFShow/Classes/General/Utils/RefreshNetServerData.m
+++ MFShow/MFShow/Classes/General/Utils/RefreshNetServerData.m
@@ -8,6 +8,38 @@
 
 #import "RefreshNetServerData.h"
 
+
+@interface RefreshNetServerData ()
+
+
+//URL
+@property (nonatomic, copy) NSString *httpUrlString;
+//MD5加密的URL字符串
+@property (nonatomic, copy) NSString *httpUrlStringMD5;
+
+
+@end
+
+
+
+
 @implementation RefreshNetServerData
+
+
+#pragma mark -  自定义初始化方法
+- (id)initWithUrlString:(NSString *)urlString {
+    self = [super init];
+    if (self) {
+        //初始化
+        self.httpUrlString = urlString;
+        self.saveBufferData = YES;
+        self.tableViewData = [NSMutableArray arrayWithCapacity:20];
+        self.showTopNoticeToastLabel = YES;
+        self.showRefreshHeaderView = YES;
+        self.showReadPostion = NO;
+        _itemMaxId = -1;
+    }
+    return self;
+}
 
 @end
